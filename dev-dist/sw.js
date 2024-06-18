9d8fe98510e503b5d07b7b520b1d79df5fffbf42
Adriel Morales <adriel8152@hotmail.com>
2024-06-18 09:55:26 -0600

    Se agrega carpeta server con nest y prisma

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.tv8td2moe0o"
+    "revision": "0.h766p268ah"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
